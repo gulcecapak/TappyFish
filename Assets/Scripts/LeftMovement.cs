b0f1cf4e9618a07514b991ccb5778897102f8efb
Gulce Capak <gulcecapak@gmail.com>
2023-05-24 23:26:16 +0300

    Added Obstacles

---
+++ Assets/Scripts/LeftMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovement : MonoBehaviour
+{
+    public float speed;
+    BoxCollider2D box;
+    float groundWidht;
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        groundWidht = box.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+        if(transform.position.x <= -groundWidht)
+        {
+            transform.position = new Vector2(transform.position.x + 2 * groundWidht, transform.position.y);
+        }
+    }
+}
